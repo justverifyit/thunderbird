a6362ea9cc6ffd75bf37914c15d673abb2a68b62
Nicolas Provencher <nicolas@Nicolass-Mac-mini.local>
2023-04-16 15:44:01 -0400

    Initial POC

---
+++ src/scripts/background.js
@@ -0,0 +1,165 @@
+browser.storage.local.set({currentStatus: "none"});
+browser.storage.local.remove("shouldUpdateBanner");
+
+browser.messageDisplayScripts.register({
+    js: [
+        { file: "scripts/message-script.js" }
+    ],
+    css: [
+        { file: "scripts/message-script.css" }
+    ]
+})
+
+browser.messageDisplay.onMessageDisplayed.addListener(async (tab, message) => {
+    // Get the current message ID from the message object
+    let newMessageId = message.id;
+
+    // Retrieve the previously displayed message ID from storage
+    let { currentMessageId } = await browser.storage.local.get("currentMessageId");
+
+    // Check if the current message ID is different from the previous message ID
+    if (currentMessageId !== newMessageId) {
+        console.log("A different message is clicked");
+
+        // Clear status
+        await browser.storage.local.set({currentStatus: "none"});
+    }
+});
+
+messenger.messageDisplayAction.onClicked.addListener(async (tab) =>{
+    browser.messageDisplay.getDisplayedMessage(tab.id).then(async (message) => {
+        await browser.storage.local.set({currentMessageId: message.id});
+
+        // Check current status
+        let { currentStatus } = await browser.storage.local.get("currentStatus");
+
+        newStatus = "loading"
+
+        if (newStatus !== currentStatus) {
+            await browser.storage.local.set({currentStatus: newStatus});
+            await browser.storage.local.set({shouldUpdateBanner: "true"});
+        }
+
+        onButtonClick(tab.id, message);
+    });
+});
+
+async function onButtonClick(tabId, message) {
+    const attachments = await browser.messages.listAttachments(message.id);
+
+    // Scan attachments with VirusTotal
+    let results = [];
+    for (let attachment of attachments) {
+        const result = await scanWithVirusTotal(message, attachment);
+        results.push(result);
+    }
+
+    showResults(results);
+}
+
+async function scanWithVirusTotal(message, attachment) {
+    try {
+        const result = await performScanAndPolling(message, attachment);
+        return {
+            filename: attachment.name,
+            result,
+        };
+    } catch (error) {
+        return {
+            filename: attachment.name,
+            error: error.message,
+        };
+    }
+}
+
+async function performScanAndPolling(message, attachment) {
+    const apiKey = "55fb998c6550d06fce29f5d06e44c42baa3f639be15ca87bbf02c50e88f8be20";
+    const apiUrl = "https://www.virustotal.com/api/v3/files";
+
+    // Download the attachment
+    let attachmentBlob = await messenger.messages.getAttachmentFile(message.id, attachment.partName);
+    let formData = new FormData();
+    formData.append("file", attachmentBlob);
+
+
+    // Upload the attachment to VirusTotal
+    response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "x-apikey": apiKey,
+        },
+        body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error uploading to VirusTotal: ${response.statusText}`);
+    }
+
+    // Get the analysis ID
+    let data = await response.json();
+    const analysisId = data.data.id;
+
+    // Poll for the analysis result
+    return await pollVirusTotalResult(apiKey, analysisId);
+}
+
+async function pollVirusTotalResult(apiKey, analysisId) {
+    const delayBetweenRetries = 250; // 250ms
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    let retries = 5;
+
+    while (retries > 0) {
+        try {
+            return await performPolling(apiKey, analysisId);
+        } catch (error) {
+            retries--;
+            if (retries === 0) {
+                throw new Error("Error fetching analysis result: Max retries reached");
+            }
+            await new Promise((resolve) => setTimeout(resolve, 15000));
+        }
+        await delay(delayBetweenRetries);
+    }
+}
+
+async function performPolling(apiKey, analysisId) {
+    const apiUrl = `https://www.virustotal.com/api/v3/analyses/${analysisId}`;
+
+    const response = await fetch(apiUrl, {
+        headers: {
+            "x-apikey": apiKey,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching analysis result: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const status = data.data.attributes.status;
+
+    if (status === "completed") {
+        return data.data.attributes;
+    } else if (status === "failed") {
+        throw new Error("VirusTotal analysis failed");
+    } else {
+        throw new Error("Analysis status unknown");
+    }
+}
+
+async function showResults(results){
+    let foundMalicious = false;
+    results.forEach(function (result, _) {
+        if (result.result.stats.malicious > 0 || result.result.stats.suspicious > 0){
+            foundMalicious = true;
+            browser.storage.local.set({currentStatus: "danger"});
+            browser.storage.local.set({shouldUpdateBanner: "true"});
+        }
+    })
+
+    if (!foundMalicious){
+        await browser.storage.local.set({currentStatus: "safe"});
+        await browser.storage.local.set({shouldUpdateBanner: "true"});
+    }
+}
---
+++ src/scripts/message-script.js
@@ -0,0 +1,53 @@
+function addBanner(status){
+    // create the notification element itself
+    const notification = document.createElement("div");
+
+    // create the notification text element
+    const notificationText = document.createElement("div");
+    notificationText.className = "verifyit_banner_text";
+
+    // Set proper banner
+    switch (status){
+        case "safe":
+            notification.className = "verifyit_banner banner success-banner";
+            notificationText.innerText = "No viruses detected in any of the attachments!";
+            break;
+        case "loading":
+            notification.className = "verifyit_banner banner loading-banner";
+            notificationText.innerText = "Loading...";
+            break;
+        case "danger":
+            notification.className = "verifyit_banner banner danger-banner";
+            notificationText.innerText = "One or more of the attachments appear to contain a virus, proceed with caution!";
+            break;
+    }
+
+    // add text and button to the notification
+    notification.appendChild(notificationText);
+
+    // and insert it as the very first element in the message
+    document.body.insertBefore(notification, document.body.firstChild);
+}
+const showNotification = async () => {
+
+    let { shouldUpdateBanner } = await browser.storage.local.get("shouldUpdateBanner");
+    let existingBanner = document.querySelector(".verifyit_banner");
+
+    // Check if there is an update
+    if (shouldUpdateBanner) {
+        // Clear the update flag
+        await browser.storage.local.remove("shouldUpdateBanner");
+
+        // Get current status
+        let { currentStatus } = await browser.storage.local.get("currentStatus");
+
+        // Remove existing banner if it exist
+        if (existingBanner)
+            existingBanner.remove();
+
+        addBanner(currentStatus);
+    }
+};
+
+// Poll the browser.storage.local for changes every 500 milliseconds
+setInterval(showNotification, 50);
