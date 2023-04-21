39def1c8f87ea8e64e706d15ca4325b9ff90165b
Nicolas Provencher <nicolas@Nicolass-Mac-mini.local>
2023-04-21 11:58:10 -0400

    Add english locales + adjust script to uses locales

--- src/options/js/options.js
+++ src/options/js/options.js
@@ -1,3 +1,24 @@
+// Locales setup
+$("#tab1").html(browser.i18n.getMessage("general.tab.1"));
+$("#tab2").html(browser.i18n.getMessage("general.tab.2"));
+
+$("#menu1 #title").html(browser.i18n.getMessage("options.menu1.title"));
+$("#menu1 #description").html(browser.i18n.getMessage("options.menu1.description"));
+$("#menu1 #label1").html(browser.i18n.getMessage("options.menu1.label1"));
+$("#menu1 #help").html(browser.i18n.getMessage("options.menu1.help"));
+$("#menu1 #alertSuccess").html(browser.i18n.getMessage("options.menu1.alertSuccess"));
+$("#menu1 #submit").html(browser.i18n.getMessage("options.menu1.submit"));
+
+$("#menu2 #title").html(browser.i18n.getMessage("options.menu2.title"));
+$("#menu2 #description").html(browser.i18n.getMessage("options.menu2.description"));
+$("#menu2 #label1").html(browser.i18n.getMessage("options.menu2.label1"));
+$("#menu2 #delayHelp").html(browser.i18n.getMessage("options.menu2.delayHelp"));
+$("#menu2 #label2").html(browser.i18n.getMessage("options.menu2.label2"));
+$("#menu2 #retryHelp").html(browser.i18n.getMessage("options.menu2.retryHelp"));
+$("#menu2 #alertSuccess2").html(browser.i18n.getMessage("options.menu2.alertSuccess"));
+$("#menu1 #alertDanger2").html(browser.i18n.getMessage("options.menu2.alertDanger"));
+$("#menu2 #submit").html(browser.i18n.getMessage("options.menu2.submit"));
+
 // Main Interface
 $("li").on("click", function(){
     $('#alertSuccess').hide();
